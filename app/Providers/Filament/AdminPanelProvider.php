600656a2c2c7a8347fbe938709a9f0df2c2a4723
wallace martins <wallacemartinss@gmail.com>
2025-01-02 12:45:40 -0300

    adção do profile

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->databaseNotifications()
+            ->profile()
             ->sidebarFullyCollapsibleOnDesktop()
             ->login()
             ->font('Inter')
